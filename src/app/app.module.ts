45a00554bd76bc8a7e9c348bd137e08a6681b3ab
Kamzs <kamilambroziak@interia.pl>
2019-12-27 21:54:52 +0100

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Kliker1Component } from './kliker1/kliker1.component';
 import { Kliker2Component } from './kliker2/kliker2.component';
-import { KlikServiceService } from './services/klik-service.service';
-import { LoggerService } from './services/logger.service';
+
 
 @NgModule({
   declarations: [
